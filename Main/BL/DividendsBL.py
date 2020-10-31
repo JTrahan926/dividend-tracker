352809885cf36a6737767022c8fd39ee3b97af12
Jaden <jadentrahan@gmail.com>
2020-10-30 20:14:48 -0500

    Don't remember what all I changed lol

--- Main/BL/DividendsBL.py
+++ Main/BL/DividendsBL.py
@@ -2,14 +2,13 @@ import os.path
 import pandas as pd
 import yfinance as yf
 
-DIVIDENDS_CSV_PATH = '../../Resources/OutputFiles/Dividends/'
-
 def getDividendHistory(ticker: str):
     """ Gets the dividend history of a given ticker from yfinance
-    
+
     :param ticker: (str) NYSE stock ticker
     :return: pandas DataFrame of dividends and dates the dividend was paid out for the given stock
     """
+
     dividends_ser = yf.Ticker(ticker).dividends
     df = pd.DataFrame(dividends_ser).reset_index()
     df.columns = ['Date','Dividends']
--- Main/GUI/MainAppLayout.py
+++ Main/GUI/MainAppLayout.py
@@ -5,17 +5,15 @@ from dash.dependencies import Input, Output
 import BL.DividendsBL as divBL
 import GUI.GUICentral as gui
 import plotly.graph_objs as go
+import pandas as pd
 
 def build():
     mainWindow = dash.Dash()
-    ticker1 = 'PEP'
-    ticker2 = 'KO'
-    dividends_test1 = divBL.getDividendHistory(ticker1)
-    dividends_test2 = divBL.getDividendHistory(ticker2)
-
+    
     mainWindow.layout = dash_html.Div(
         style={
             'backgroundColor': gui.Colors.BACKGROUND,
+            'align-items':'center'
         },
         children=[
             #title
@@ -27,127 +25,159 @@ def build():
                     }
                 ),
             #subtitle
-                dash_html.Div(children='This graph compares dividend history of {} and {}'.format(ticker1,ticker2),
-                              style={
+                dash_html.Div(  
+                                children='Select a Ticker from the Dropdown',
+                                style={
+                                  #'width': '49%',
+                                  #'display': 'inline-block',
                                   'textAlign': 'center',
                                   'color': gui.Colors.TEXT
                               }),
-            #graph
+                # Ticker Dropdown
                 dash_html.Div(
-                    children=[
-                                dash_cc.Graph(
-                                    id='Graph1',
-                                    figure={
-                                        'data': [
-                                            {'x': dividends_test1['Date'],
-                                             'y': dividends_test1['Dividends'],
-                                             'type': 'line',
-                                             'name': ticker1,
-                                             'mode': 'lines',
-                                             'line': {
-                                                 'color': gui.Colors.BAD_RED,
-                                                 'width':3
-                                             }
-                                             },
-                                            {'x': dividends_test2['Date'],
-                                             'y': dividends_test2['Dividends'],
-                                             'type': 'line',
-                                             'name': ticker2,
-                                             'mode': 'lines',
-                                             'line': {
-                                                 'color': gui.Colors.GOOD_GREEN,
-                                                 'width':3
-                                                }
-                                             },
-                                        ],
-                                        'layout': {
-                                            'plot_bgcolor': gui.Colors.GRAPH_BACKGROUND,
-                                            'paper_bgcolor': gui.Colors.BACKGROUND,
-                                            'font': {
-                                            'color': gui.Colors.TEXT
-                                            },
-                                            'xaxis': {
-                                                'gridcolor': gui.Colors.TEXT
-                                            },
-                                            'yaxis': {
-                                                'gridcolor': gui.Colors.TEXT
-                                            }
-                                        }
-                                    }
+                            style={'align-items':'center',
+                                    'justify-content':'center'},
+                            children=[
+                            dash_cc.Dropdown(
+                                id='ticker-dropdown',
+                                style={'width':'30%'},
+                                options=generateDropdownOptions(),
+                                value=generateDropdownOptions()[0]['value']
                                 )
-                    ],
-                    style={
-                        'width':'70%',
-                        #'border':'2px solid '+gui.Colors.TEXT,
-                        'margin':15
-                    }
-                )
+                            ]),
+                dash_html.Div(
+                            id='ticker-details',
+                            style= {
+                                    #'width': '49%',
+                                    #'display': 'inline-block',
+                                    'margin':15
+                                }
+                            )
+            #graph
+            #createComparisonGraph(ticker1,ticker2)
+
         ])
 
-        # style={'backgroundColor': gui.Colors.BACKGROUND},
-        #
-        # children=[
-        #
-        #
-        #     dash_html.H1(
-        #         children='Dividends Graph Test',
-        #         style={
-        #             'textAlign': 'center',
-        #             'color': gui.Colors.TEXT
-        #         }
-        #     ),
-        #
-        #
-        #     dash_html.Div(children='This graph compares dividend history of {} and {}'.format(ticker1,ticker2),
-        #                   style={
-        #                       'textAlign': 'center',
-        #                       'color': gui.Colors.TEXT
-        #                   }),
-        #
-        #
-        #     dash_html.Div(children=[
-        #         dash_cc.Graph(
-        #             id='Graph1',
-        #             figure={
-        #                 'data': [
-        #                     {'x': dividends_test1['Date'],
-        #                      'y': dividends_test1['Dividends'],
-        #                      'type': 'line',
-        #                      'name': ticker1,
-        #                      'marker': {'color': gui.Colors.TEXT,
-        #                                 'line': {'width': '3'}
-        #                                 }
-        #                      },
-        #                     {'x': dividends_test2['Date'],
-        #                      'y': dividends_test2['Dividends'],
-        #                      'type': 'line',
-        #                      'name': ticker2,
-        #                      'marker': {
-        #                          'size': 12,
-        #                          'color': gui.Colors.GOOD_GREEN,
-        #                          'line': {'weight': 50}
-        #                      }
-        #                      },
-        #                 ],
-        #                 'layout': {
-        #                     'plot_bgcolor': gui.Colors.GRAPH_BACKGROUND,
-        #                     'paper_bgcolor': gui.Colors.BACKGROUND,
-        #                     'font': {
-        #                         'color': gui.Colors.TEXT
-        #                     },
-        #                     'xaxis': {
-        #                         'gridcolor': gui.Colors.TEXT
-        #                     },
-        #                     'yaxis': {
-        #                         'gridcolor': gui.Colors.TEXT
-        #                     }
-        #                 }
-        #             }
-        #         )
-        #
-        #
-        #     ]
-        #     style={}])
-        # )
+    @mainWindow.callback(
+    Output(component_id='ticker-details', component_property='children'),
+    [Input(component_id='ticker-dropdown', component_property='value')]
+    )
+    def updateTickerDetails(input_value):
+        ticker = input_value
+        return dash_html.Div(
+                style={
+                    #'width': '49%',
+                    #'display': 'inline-block',
+                    #'border':'2px solid '+gui.Colors.TEXT,
+                    'margin':15
+                },
+                children=[
+                    dash_html.H2(
+                        children='Overview for {}'.format(ticker),
+                        style={
+                            'textAlign': 'center',
+                            'color': gui.Colors.TEXT
+                        }
+                    ),
+                    createTickerHistoryGraph(ticker)                         
+                ]
+            )
 
     return mainWindow
+
+def generateDropdownOptions():
+    dividendTickers = pd.read_csv('../Resources/dividendPortfolioData.csv')['Ticker']
+    options = []
+    for ticker in dividendTickers:
+        options.append({'label':ticker,'value':ticker})
+    return options
+
+def createTickerHistoryGraph(ticker):
+    dividends = divBL.getDividendHistory(ticker)
+    return dash_cc.Graph(
+                                id='Graph1',
+                                figure={
+                                    'data': [
+                                        {'x': dividends['Date'],
+                                         'y': dividends['Dividends'],
+                                         'type': 'line',
+                                         'name': ticker,
+                                         'mode': 'lines',
+                                         'line': {
+                                             'color': gui.Colors.BAD_RED,
+                                             'width':3
+                                         }
+                                         }
+                                        ],
+                                    'layout': {
+                                        'plot_bgcolor': gui.Colors.GRAPH_BACKGROUND,
+                                        'paper_bgcolor': gui.Colors.BACKGROUND,
+                                        'font': {
+                                        'color': gui.Colors.TEXT
+                                        },
+                                        'xaxis': {
+                                            'gridcolor': gui.Colors.TEXT
+                                        },
+                                        'yaxis': {
+                                            'gridcolor': gui.Colors.TEXT
+                                        }
+                                    }
+                                }
+                            )
+
+
+
+
+def createComparisonGraph(ticker1, ticker2):
+    dividends_test1 = divBL.getDividendHistory(ticker1)
+    dividends_test2 = divBL.getDividendHistory(ticker2)
+    return dash_html.Div(
+                children=[
+                            dash_cc.Graph(
+                                id='Graph1',
+                                figure={
+                                    'data': [
+                                        {'x': dividends_test1['Date'],
+                                         'y': dividends_test1['Dividends'],
+                                         'type': 'line',
+                                         'name': ticker1,
+                                         'mode': 'lines',
+                                         'line': {
+                                             'color': gui.Colors.BAD_RED,
+                                             'width':3
+                                         }
+                                         },
+                                        {'x': dividends_test2['Date'],
+                                         'y': dividends_test2['Dividends'],
+                                         'type': 'line',
+                                         'name': ticker2,
+                                         'mode': 'lines',
+                                         'line': {
+                                             'color': gui.Colors.GOOD_GREEN,
+                                             'width':3
+                                        }
+                                         },
+                                        ],
+                                    'layout': {
+                                        'plot_bgcolor': gui.Colors.GRAPH_BACKGROUND,
+                                        'paper_bgcolor': gui.Colors.BACKGROUND,
+                                        'font': {
+                                        'color': gui.Colors.TEXT
+                                        },
+                                        'xaxis': {
+                                            'gridcolor': gui.Colors.TEXT
+                                        },
+                                        'yaxis': {
+                                            'gridcolor': gui.Colors.TEXT
+                                        }
+                                    }
+                                }
+                            )
+                ],
+                style={
+                    'width': '49%',
+                    'display': 'inline-block',
+                    #'border':'2px solid '+gui.Colors.TEXT,
+                    'margin':15
+                }
+            )
